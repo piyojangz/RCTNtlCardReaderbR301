4fedbe006ae20e710cfe1498ce68b6da95c33b30
Breeshy Sama <breesh.comsci@gmail.com>
2018-12-04 14:02:51 +0700

    Initial Commit

---
+++ RCTNtlCardReader/RCTNtlCardReader.h
@@ -0,0 +1,13 @@
+//
+//  RCTNtlCardReader.h
+//  RCTNtlCardReader
+//
+//  Created by Breeshy Sama on 4/12/2561 BE.
+//  Copyright © 2561 Ngern Tid Lor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCTNtlCardReader : NSObject
+
+@end
---
+++ RCTNtlCardReader/RCTNtlCardReader.m
@@ -0,0 +1,13 @@
+//
+//  RCTNtlCardReader.m
+//  RCTNtlCardReader
+//
+//  Created by Breeshy Sama on 4/12/2561 BE.
+//  Copyright © 2561 Ngern Tid Lor. All rights reserved.
+//
+
+#import "RCTNtlCardReader.h"
+
+@implementation RCTNtlCardReader
+
+@end
